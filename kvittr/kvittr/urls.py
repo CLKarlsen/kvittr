c330cdbae011c29db1ca641304f31fe35de1957d
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-23 00:30:18 +0200

    Added an url in kvittr/urls.py that includes the urls in the kvittr_messages application.

--- kvittr/kvittr/urls.py
+++ kvittr/kvittr/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('theme.urls')),
     url(r'^useraccounts/', include ('useraccounts.urls')),
+    url(r'^kvittr_messages/', include('kvittr_messages.urls'))
 )
