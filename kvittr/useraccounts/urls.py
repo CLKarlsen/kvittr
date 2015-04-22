ff1b63f288cb63a52e6d2620a001a0fbedb31424
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-23 01:56:12 +0200

    Added a url to the user_profile page in urls.py

--- kvittr/useraccounts/urls.py
+++ kvittr/useraccounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
 	url(r'^register$', views.user_register, name='user_register'),
 	url(r'^login$', views.user_login, name='user_login'),
 	url(r'^logout$', views.user_logout, name='user_logout'),
+	url(r'^user_profile$', views.user_profile, name='user_profile'),
 	)
