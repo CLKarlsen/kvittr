ae22eff3aafd5c6a644c57466a5920f4fbd8ad88
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-22 14:15:22 +0200

    Added url patterns to the kvittr/urls.py file.

--- kvittr/kvittr/urls.py
+++ kvittr/kvittr/urls.py
@@ -1,12 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'kvittr.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns ('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include('theme.urls')),
     url(r'^useraccounts/', include ('useraccounts.urls')),
-]
+)
