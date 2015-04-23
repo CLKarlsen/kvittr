d080d1f753b2920e4bec63e3eb60a7c79bd5c95f
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-24 00:03:51 +0200

    By mistake I used space instead of tab when I made indentation in views.py.

--- kvittr/kvittr_messages/views.py
+++ kvittr/kvittr_messages/views.py
@@ -6,6 +6,6 @@ from kvittr_messages.models import Message
 # Create your views here.
 def message_listing(request):
 	messages = Message.objects.all()
-    #context = {'messages': messages}
-    #return render(request, 'kvittr_messages/message_listing.html', context)
-	return HttpResponse("It worked!")
+	context = {'messages': messages}
+	return render(request, 'kvittr_messages/message_listing.html', context)
+	#return HttpResponse("It worked!")
