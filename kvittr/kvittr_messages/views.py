965d32fac3d7b0bb22c3eb82aca52ca7b6ab30af
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-24 23:43:32 +0200

    Added a paginator function in views, so each page displays 5 messages.

--- kvittr/kvittr_messages/views.py
+++ kvittr/kvittr_messages/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from kvittr_messages.models import Message
 
 # Create your views here.
 def message_listing(request):
 	messages = Message.objects.all()
+
+	page_number = request.GET.get('page')
+	paginator = Paginator(messages, 5)
+	try:
+		messages = paginator.page(page_number)
+	except PageNotAnInteger:
+		messages = paginator.page(1)
+	except EmptyPage:
+		messages = paginator.page(paginator.num_pages)
+
+
+
 	context = {'messages': messages}
 	return render(request, 'kvittr_messages/message_listing.html', context)
 	#return HttpResponse("It worked!")
