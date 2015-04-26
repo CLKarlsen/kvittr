b55804bc122078ff9c8965164b62cd5573caee85
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-27 00:19:31 +0200

    Added a function for the "add post" form, so the users can add new messages.

--- kvittr/kvittr_messages/views.py
+++ kvittr/kvittr_messages/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils import timezone
 
 from kvittr_messages.models import Message
 
 # Create your views here.
 def message_listing(request):
-	messages = Message.objects.all()
 
+	new_post = request.POST.get('new_post')
+	if new_post:
+		post = Message()
+		post.message = new_post
+		post.created_datetime = timezone.now()
+		post.created_by = request.user
+		post.save()
+
+	messages = Message.objects.all()
 	page_number = request.GET.get('page')
 	paginator = Paginator(messages, 5)
 	try:
