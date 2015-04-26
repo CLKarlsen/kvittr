13f8cae5480a3db39eb25e80a1025d69f49c0bdb
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-26 22:23:16 +0200

    Updated the database with the "likes" section.

---
+++ kvittr/kvittr_messages/migrations/0002_auto_20150426_2022.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('kvittr_messages', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='message',
+            options={'ordering': ['-id']},
+        ),
+        migrations.AddField(
+            model_name='message',
+            name='likes',
+            field=models.PositiveIntegerField(default=0),
+        ),
+    ]
