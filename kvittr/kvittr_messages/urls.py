4c474b5e78187321465d6c165d61f3fe047bf336
Caroline Karlsen <caroline.karlsen53@gmail.com>
2015-04-30 13:39:35 +0200

    Created a url for the detail page.

--- kvittr/kvittr_messages/urls.py
+++ kvittr/kvittr_messages/urls.py
@@ -4,5 +4,6 @@ from kvittr_messages import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.message_listing, name='message_listing'),
+	url(r'^(\d+)/$', views.message_details, name='message_details'),
 	url(r'^(\d+)/add_likes$', views.add_likes, name='add_likes'),
 )
